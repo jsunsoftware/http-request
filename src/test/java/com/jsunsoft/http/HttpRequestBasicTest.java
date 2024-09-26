595a5751247b591db095053a15854b39497b14bc
Beno Arakelyan <beno.arakelyan@gmail.com>
2024-09-26 19:28:07 +0400

    Improve HttpRequestBasicTest
    Change-Id: I23db8adc5aaa188d3cb9292d787dc7d7cc03a21e

--- src/test/java/com/jsunsoft/http/HttpRequestBasicTest.java
+++ src/test/java/com/jsunsoft/http/HttpRequestBasicTest.java
@@ -24,41 +24,16 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class HttpRequestBasicTest {
 
-    private static final HttpRequest HTTP_REQUEST_TO_GET_RESPONSE_CODE =
+    private static final HttpRequest HTTP_REQUEST =
             HttpRequestBuilder.create(new ClientBuilder().build()).build();
-
-    private static final HttpRequest HTTP_REQUEST_TO_GET_LARGE_RESPONSE =
-            HttpRequestBuilder.create(new ClientBuilder().build()).build();
-
-    @Test
-    void getResponseCode() {
-        assertEquals(HttpStatus.SC_OK, HTTP_REQUEST_TO_GET_RESPONSE_CODE.target("https://en.wikipedia.org/")
-                .path("wiki/List_of_least_concern_birds")
-                .get()
-                .getCode());
-    }
 
     @Test
     void largeResponseTest() {
-        ResponseHandler<String> responseHandler = HTTP_REQUEST_TO_GET_LARGE_RESPONSE.target("https://en.wikipedia.org/")
+        ResponseHandler<String> rh = HTTP_REQUEST.target("https://en.wikipedia.org/")
                 .path("wiki/List_of_least_concern_birds")
                 .get(String.class);
-        assertTrue(responseHandler.orElse("").length() > 16348);
-    }
 
-    @Test
-    void getResponseCodeImmutable() {
-        assertEquals(HttpStatus.SC_OK, HTTP_REQUEST_TO_GET_RESPONSE_CODE.immutableTarget("https://en.wikipedia.org/")
-                .path("wiki/List_of_least_concern_birds")
-                .get()
-                .getCode());
-    }
-
-    @Test
-    void largeResponseTestImmutable() {
-        ResponseHandler<String> responseHandler = HTTP_REQUEST_TO_GET_LARGE_RESPONSE.immutableTarget("https://en.wikipedia.org/")
-                .path("wiki/List_of_least_concern_birds")
-                .get(String.class);
-        assertTrue(responseHandler.orElse("").length() > 16348);
+        assertEquals(HttpStatus.SC_OK, rh.getCode());
+        assertTrue(rh.orElse("").length() > 16348);
     }
 }
